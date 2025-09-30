df9b0ffdd2f0ceba50e7694aa406dedb76362ef8
Kedar Saraf <kedar.saraf@mannlowe.com>
2025-09-30 22:44:39 +0530

    changes

--- app/business-dashboard/job-postings/jobPostingModal.tsx
+++ app/business-dashboard/job-postings/jobPostingModal.tsx
@@ -1489,7 +1489,7 @@ export default function JobPostingModal({ showModal, setShowModal, onSubmit, edi
               );
             })
           ) : (
-            <p className="text-sm text-gray-500 italic">No primary skills select</p>
+            <p className="text-sm text-gray-500 italic">No primary skills selected</p>
           )}
         </div>
       </div>
